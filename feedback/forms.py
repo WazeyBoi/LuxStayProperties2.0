b6004f2087b8c661d344bf0b4d24b5f1849debb6
KDazai <feerdeellaban556@gmail.com>
2024-11-29 03:40:05 +0800

    Reapply "Reapply "Tenant side of feedback and maitenance fixed""
    This reverts commit 5e583fb71844dbfe2e589661550b3d034154d74e.

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Feedback
-
+from django.core.exceptions import ValidationError
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
-        fields = ['starRating', 'comments'] 
+        fields = ['starRating', 'comments']
+
+    def clean_starRating(self):
+        starRating = self.cleaned_data.get('starRating')
+        if starRating not in range(1, 6):  # Assuming a 1-5 scale
+            raise ValidationError('Invalid star rating.')
+        return starRating
